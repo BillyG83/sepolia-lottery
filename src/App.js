3a26eaf28d977a819c4cb1664eb5924be74a7d28
billgil <bill@moralis.io>
2024-03-07 16:35:39 +0000

    feat(App): made it pretty

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import lottery from './lottery'
 import { useEffect, useState } from "react";
 import web3 from './web3';
+import './App.css'
 
 const App = () => {
   const [balance, setBalance] = useState('')
@@ -34,6 +35,14 @@ const App = () => {
     }
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target;
+    const input = form[0]
+    console.log(input.value);
+    form.reset()
+  }
+
   useEffect(() => {
     getBalance().then(res => setBalance(res))
     getManager().then(res => setManager(res))
@@ -41,11 +50,33 @@ const App = () => {
   }, [])
 
   return(
-    <div>
-      <h1>Lottery</h1>
-      <p>manager: {manager}</p>
-      <p>Number of player this round: {players.length} with a prize pool of: {balance ? web3.utils.fromWei(balance, 'ether') : 'nada'}</p>
-    </div>
+    <main>
+      <div className='dapp'>
+        <h1>Sepolia Lottery</h1>
+        <p className='subtitle'>Here we go for another excited round!</p>
+        <div className='info'>
+          <div className='icon'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
+            </svg>
+            <p>Players: {players.length}</p>
+          </div>
+          <div className='icon'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+              <path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"/>
+            </svg>
+            <p>Prize: {balance ? `${web3.utils.fromWei(balance, 'ether')}eth` : '0eth'}</p>
+          </div>
+        </div>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <h2>Feeling lucky punk?</h2>
+          <p>Join the Sepolia lottery, drawn each Sunday evening at sundown</p>
+          <input placeholder='enter your wallet address' />
+          <button type="submit">join</button>
+        </form>
+        <p className='small-print'>Game manager: {manager}</p>
+      </div>
+    </main>
   )
 }
 
